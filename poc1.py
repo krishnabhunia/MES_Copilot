b2e50c4c6062aaecff95938867e6b2081593baca
Krishna Bhunia <bhunia.k@pg.com>
2024-10-08 18:40:38 +0530

    move

--- poc1.py
+++
@@ -1,9 +0,0 @@
-from langchain_core.prompts import PromptTemplate
-
-print("Prompting the user to tell a joke about a topic")
-topic = "cats"
-prompt_template = PromptTemplate.from_template("Tell me a joke about {topic}")
-print(prompt_template)
-
-prompt_template.invoke({"topic": "cats"})
-print("User propmted templet invoked")
